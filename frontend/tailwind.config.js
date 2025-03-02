98e51c02d7885cf922956f41d46979dd454a820f
Tane4ka170 <tane4ka170@gmail.com>
2025-03-02 17:38:18 +0200

    feat: added paleBlue colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,19 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      paleBlue: {
+        100: "#efeffa",
+        200: "#dfdef5",
+        300: "#cfcef1",
+        400: "#bfbdec",
+        500: "#afade7",
+        600: "#8c8ab9",
+        700: "#69688b",
+        800: "#46455c",
+        900: "#23232e",
+      },
+    },
   },
   plugins: [],
 };
