3d06eb21bed8cbbccd9198a448c23016ef229af9
Tane4ka170 <tane4ka170@gmail.com>
2025-03-03 20:06:16 +0200

    feat: added home page layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import BottomNav from "../components/shared/BottomNav";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="bg-paleBlue-400 h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* Left div */}
+      <div className="flex-[3] bg-paleBlue-700"></div>
+
+      {/* Right div */}
+      <div className="flex-[7] bg-paleBlue-700"></div>
+
+      <BottomNav />
+    </section>
+  );
 };
 
 export default Home;
