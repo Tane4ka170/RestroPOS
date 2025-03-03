b0ce740fb13de37a3a9fe02e1613c02bf4cd8824
Tane4ka170 <tane4ka170@gmail.com>
2025-03-03 15:56:19 +0200

    feat: added header search bar

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import logo from "../../assets/images/logo.png";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center py-4 px-8 bg-paleBlue-500">
+      {/* Logo */}
       <div className="flex items-center gap-2">
         <img src={logo} alt="Logo" className="h-8 w-8" />
         <h1 className="text-lg font-semibold text-paleBlue-100">Restro</h1>
+      </div>
+
+      {/* SearchBar */}
+      <div className="flex items-center gap-2 bg-paleBlue-900 p-2 rounded-[20px]">
+        <FaSearch className="text-paleBlue-100" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="bg-paleBlue-800 text-paleBlue-100 px-2 py-1 rounded-md"
+        />
       </div>
     </header>
   );
