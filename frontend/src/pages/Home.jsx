63305264bd57875f3a3d751f3428403a6fe3ab44
Tane4ka170 <tane4ka170@gmail.com>
2025-03-04 17:30:56 +0200

    feat: integrated greetings in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import BottomNav from "../components/shared/BottomNav";
+import Greetings from "../components/home/Greetings";
 
 const Home = () => {
   return (
     <section className="bg-paleBlue-400 h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
       {/* Left div */}
-      <div className="flex-[3] bg-paleBlue-700"></div>
+      <div className="flex-[3] bg-paleBlue-700">
+        <Greetings />
+      </div>
 
       {/* Right div */}
       <div className="flex-[7] bg-paleBlue-700"></div>
