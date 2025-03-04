176a321fb19adc5823956d1a39045817eb3b0eb3
Tane4ka170 <tane4ka170@gmail.com>
2025-03-04 18:29:56 +0200

    feat: added mini cards to home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import BottomNav from "../components/shared/BottomNav";
 import Greetings from "../components/home/Greetings";
+import { FaCashRegister } from "react-icons/fa";
+import { RiProgress1Line } from "react-icons/ri";
 
 const Home = () => {
   return (
@@ -8,6 +10,20 @@ const Home = () => {
       {/* Left div */}
       <div className="flex-[3] bg-paleBlue-700">
         <Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard
+            title="Total Earnings"
+            icon={<FaCashRegister />}
+            number={512}
+            footerNum={1.6}
+          />
+          <MiniCard
+            title="In Progress"
+            icon={<RiProgress1Line />}
+            number={1}
+            footerNum={3.6}
+          />
+        </div>
       </div>
 
       {/* Right div */}
