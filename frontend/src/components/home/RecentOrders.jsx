65411745f210614d12c0a34be1577e5f144e1909
Tane4ka170 <tane4ka170@gmail.com>
2025-03-05 14:59:31 +0200

    feat: styled recent orders section

--- frontend/src/components/home/RecentOrders.jsx
+++ frontend/src/components/home/RecentOrders.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const RecentOrders = () => {
   return (
-    <div className="px-8">
-      <div></div>
+    <div className="px-8 mt-6">
+      <div className="bg-paleBlue-800 w-full h-[450px] rounded-lg">
+        <div className="flex justify-between items-center px-6 py-4">
+          <h1 className="text-paleBlue-100 text-lg">Recent Orders</h1>
+          <a href="" className="text-[#de4d13] text-sm font-semibold">
+            View all
+          </a>
+        </div>
+
+        <div className="flex items-center gap-4 bg-paleBlue-900 rounded-[15px] px-6 py-4 mx-6">
+          <FaSearch className="text-paleBlue-100" />
+          <input
+            type="text"
+            placeholder="Search resent orders"
+            className="bg-paleBlue-900 text-paleBlue-100  outline-none"
+          />
+        </div>
+      </div>
     </div>
   );
 };
