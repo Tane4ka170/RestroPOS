33d0e699162504d22f715830c6e12235a46345b5
Tane4ka170 <tane4ka170@gmail.com>
2025-03-08 16:48:08 +0200

    feat: added navigation to bottom nav

--- frontend/src/components/shared/BottomNav.jsx
+++ frontend/src/components/shared/BottomNav.jsx
@@ -3,18 +3,32 @@ import { CiCircleMore, CiHome } from "react-icons/ci";
 import { GoListOrdered } from "react-icons/go";
 import { MdOutlineTableBar } from "react-icons/md";
 import { BiDish } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 const BottomNav = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-paleBlue-800 p-2 flex justify-around">
-      <button className="text-paleBlue-100 bg-paleBlue-700 w-[200px] rounded-[20px] items-center flex justify-center">
+      <button
+        className="text-paleBlue-100 bg-paleBlue-700 w-[200px] rounded-[20px] items-center flex justify-center"
+        onClick={() => navigate("/")}
+      >
         <CiHome className="inline mr-2" size={20} /> <p>Home</p>
       </button>
-      <button className="text-paleBlue-200 bg-paleBlue-800 w-[200px] rounded-[20px] items-center flex justify-center">
+      <button
+        className="text-paleBlue-200 bg-paleBlue-800 w-[200px] rounded-[20px] items-center flex justify-center"
+        onClick={() => navigate("/orders")}
+      >
         <GoListOrdered className="inline mr-2" size={20} /> <p>Orders</p>
       </button>
       <button className="text-paleBlue-200 bg-paleBlue-800 w-[200px] rounded-[20px] items-center flex justify-center">
-        <MdOutlineTableBar className="inline mr-2" size={20} /> <p>Tables</p>
+        <MdOutlineTableBar
+          className="inline mr-2"
+          size={20}
+          onClick={() => navigate("/tables")}
+        />{" "}
+        <p>Tables</p>
       </button>
       <button className="text-paleBlue-200 bg-paleBlue-800 w-[200px] rounded-[20px] items-center flex justify-center">
         <CiCircleMore className="inline mr-2" size={20} /> <p>More</p>
