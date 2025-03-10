d5135c6c848a22ce5701713cf3b1723297e67722
Tane4ka170 <tane4ka170@gmail.com>
2025-03-10 14:31:20 +0200

    feat: added back button to orders page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import BottomNav from "../components/shared/BottomNav";
 import OrderCard from "../components/orders/OrderCard";
+import BackButton from "../components/shared/BackButton";
 
 const Orders = () => {
   return (
     <section className="bg-paleBlue-400 h-[calc(100vh-5rem)] overflow-hidden">
       <div className="flex items-center justify-between px-10 py-4">
-        <h1 className="text-paleBlue-100 text-xl font-bold tracking-wider">
-          Orders
-        </h1>
+        <div>
+          <BackButton />
+          <h1 className="text-paleBlue-100 text-xl font-bold tracking-wider">
+            Orders
+          </h1>
+        </div>
         <div className="flex items-center justify-around gap-4">
           <button className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold">
             All
