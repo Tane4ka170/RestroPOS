d61d2d0fb75ec3ecb6b96f002f30ed991b8deacd
Tane4ka170 <tane4ka170@gmail.com>
2025-03-10 14:39:46 +0200

    feat: added order status filtering

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,29 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNav from "../components/shared/BottomNav";
 import OrderCard from "../components/orders/OrderCard";
 import BackButton from "../components/shared/BackButton";
 
 const Orders = () => {
+  const [status, setStatus] = useState("all");
+
   return (
     <section className="bg-paleBlue-400 h-[calc(100vh-5rem)] overflow-hidden">
       <div className="flex items-center justify-between px-10 py-4">
-        <div>
+        <div className="flex items-center gap-4">
           <BackButton />
           <h1 className="text-paleBlue-100 text-xl font-bold tracking-wider">
             Orders
           </h1>
         </div>
         <div className="flex items-center justify-around gap-4">
-          <button className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold">
+          <button
+            className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold"
+            onClick={() => setStatus("all")}
+          >
             All
           </button>
-          <button className="text-paleBlue-200 text-lg bg-paleBlue-700 rounded-lg px-4 py-2 font-semibold">
+          <button
+            className="text-paleBlue-200 text-lg bg-paleBlue-700 rounded-lg px-4 py-2 font-semibold"
+            onClick={() => setStatus("progress")}
+          >
             In Progress
           </button>
-          <button className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold">
+          <button
+            className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold"
+            onClick={() => setStatus("ready")}
+          >
             Ready
           </button>
-          <button className="text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold">
+          <button
+            className="text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold"
+            onClick={() => setStatus("completed")}
+          >
             Completed
           </button>
         </div>
