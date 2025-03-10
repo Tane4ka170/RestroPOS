629442ba88073c5aada492a0b2c4160dcc8ba466
Tane4ka170 <tane4ka170@gmail.com>
2025-03-10 18:18:32 +0200

    feat: added active state to order status buttons

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -17,25 +17,33 @@ const Orders = () => {
         </div>
         <div className="flex items-center justify-around gap-4">
           <button
-            className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold"
+            className={`text-paleBlue-200 text-lg rounded-lg px-5 py-2 font-semibold ${
+              status === "all" && "bg-paleBlue-700 rounded-lg px-5 py-2"
+            }`}
             onClick={() => setStatus("all")}
           >
             All
           </button>
           <button
-            className="text-paleBlue-200 text-lg bg-paleBlue-700 rounded-lg px-4 py-2 font-semibold"
+            className={`text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold ${
+              status === "progress" && "bg-paleBlue-700 rounded-lg px-5 py-2"
+            }`}
             onClick={() => setStatus("progress")}
           >
             In Progress
           </button>
           <button
-            className="text-paleBlue-200 text-lg  rounded-lg px-4 py-2 font-semibold"
+            className={`text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold ${
+              status === "ready" && "bg-paleBlue-700 rounded-lg px-5 py-2"
+            }`}
             onClick={() => setStatus("ready")}
           >
             Ready
           </button>
           <button
-            className="text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold"
+            className={`text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold ${
+              status === "completed" && "bg-paleBlue-700 rounded-lg px-5 py-2"
+            }`}
             onClick={() => setStatus("completed")}
           >
             Completed
