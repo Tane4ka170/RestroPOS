f1cb09673b2579d30db64eef0e0a9faea25929f4
Tane4ka170 <tane4ka170@gmail.com>
2025-03-11 16:43:31 +0200

    feat: added table card component

---
+++ frontend/src/components/tables/TableCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TableCard = () => {
+  return (
+    <div className="w-[500px] bg-paleBlue-600 p-4 rounded-lg mb-4">
+      TableCard
+    </div>
+  );
+};
+
+export default TableCard;
