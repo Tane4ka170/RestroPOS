de28a68b8e3cc498fdfbcab818ccbcff2fb9a03a
Tane4ka170 <tane4ka170@gmail.com>
2025-03-11 16:44:03 +0200

    feat: implemented table list and status filter

--- frontend/src/pages/Tables.jsx
+++ frontend/src/pages/Tables.jsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import BottomNav from "../components/shared/BottomNav";
+import BackButton from "../components/shared/BackButton";
+import TableCard from "../components/tables/TableCard";
 
 const Tables = () => {
+  const [status, setStatus] = useState("all");
+
   return (
     <section className="bg-paleBlue-400 h-[calc(100vh-5rem)] overflow-hidden">
       <div className="flex items-center justify-between px-10 py-4">
         <div className="flex items-center gap-4">
           <BackButton />
           <h1 className="text-paleBlue-100 text-xl font-bold tracking-wider">
-            Orders
+            Tables
           </h1>
-        </div>{" "}
+        </div>
+
+        <div className="flex items-center justify-around gap-4">
+          <button
+            className={`text-paleBlue-200 text-lg rounded-lg px-5 py-2 font-semibold ${
+              status === "all" && "bg-paleBlue-700 rounded-lg px-5 py-2"
+            }`}
+            onClick={() => setStatus("all")}
+          >
+            All
+          </button>
+          <button
+            className={`text-paleBlue-200 text-lg rounded-lg px-4 py-2 font-semibold ${
+              status === "booked" && "bg-paleBlue-700 rounded-lg px-5 py-2"
+            }`}
+            onClick={() => setStatus("booked")}
+          >
+            Booked
+          </button>
+        </div>
+
+        <div className="px-16 py-4 ">
+          <TableCard />
+        </div>
       </div>
       <BottomNav />
     </section>
