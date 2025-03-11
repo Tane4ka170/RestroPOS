8549b9603c3776508ed0b9ea1f5c42833be562bf
Tane4ka170 <tane4ka170@gmail.com>
2025-03-11 16:30:28 +0200

    feat: added tables route to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Router, Routes } from "react-router-dom";
-import { Auth, Home, Orders } from "./pages";
+import { Auth, Home, Orders, Tables } from "./pages";
 import Header from "./components/shared/Header";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
         <Route path={"/"} element={<Home />} />
         <Route path={"/auth"} element={<Auth />} />
         <Route path={"/orders"} element={<Orders />} />
+        <Route path={"/tables"} element={<Tables />} />
       </Routes>
     </>
   );
