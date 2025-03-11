5d717ee1fc39ad711500484daefc5a8969df0b71
Tane4ka170 <tane4ka170@gmail.com>
2025-03-11 16:59:32 +0200

    feat: styled table card component

--- frontend/src/components/tables/TableCard.jsx
+++ frontend/src/components/tables/TableCard.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const TableCard = () => {
   return (
-    <div className="w-[500px] bg-paleBlue-600 p-4 rounded-lg mb-4">
-      TableCard
+    <div className="w-[300px] bg-paleBlue-600 p-4 rounded-lg mb-4 cursor-pointer shadow-lg">
+      <div className="flex items-center justify-between px-1">
+        <h1 className="text-paleBlue-100 text-xl font-semibold">Table 1</h1>
+        <p className="text-green-600 px-2 bg-green-300 rounded-lg py-1">
+          Booked
+        </p>
+      </div>
+      <div className="flex items-center justify-between my-5">
+        <h1 className="bg-[#74bebb] text-paleBlue-100 rounded-full p-4 text-xl">
+          TS
+        </h1>
+      </div>
     </div>
   );
 };
