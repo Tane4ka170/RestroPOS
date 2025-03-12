c7197ba19963b08729172d6be407135081844e1e
Tane4ka170 <tane4ka170@gmail.com>
2025-03-12 19:08:45 +0200

    feat: added dynamic data to table card component

--- frontend/src/components/tables/TableCard.jsx
+++ frontend/src/components/tables/TableCard.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 
-const TableCard = () => {
+const TableCard = ({ key, name, status, initials }) => {
   return (
-    <div className="w-[300px] bg-paleBlue-600 p-4 rounded-lg mb-4 cursor-pointer shadow-lg">
+    <div
+      className="w-[300px] bg-paleBlue-600 p-4 rounded-lg cursor-pointer"
+      key={key}
+    >
       <div className="flex items-center justify-between px-1">
-        <h1 className="text-paleBlue-100 text-xl font-semibold">Table 1</h1>
-        <p className="text-green-600 px-2 bg-green-300 rounded-lg py-1">
-          Booked
+        <h1 className="text-paleBlue-100 text-xl font-semibold">{name}</h1>
+        <p
+          className={`${
+            status === "Booked"
+              ? "text-green-600 bg-green-300"
+              : "bg-aquaTeal-800 text-paleBlue-100"
+          } px-2  rounded-lg py-1`}
+        >
+          {status}
         </p>
       </div>
-      <div className="flex items-center justify-between my-5">
-        <h1 className="bg-[#74bebb] text-paleBlue-100 rounded-full p-4 text-xl">
-          TS
+      <div className="flex items-center justify-between mt-5 mb-7">
+        <h1
+          className={`bg-aquaTeal-500 text-paleBlue-100 rounded-full p-4 text-xl`}
+        >
+          {initials}
         </h1>
       </div>
     </div>
