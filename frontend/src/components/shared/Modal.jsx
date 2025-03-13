d98a43369425017dc0f50594e6098de28dd6dad2
Tane4ka170 <tane4ka170@gmail.com>
2025-03-13 19:36:02 +0200

    feat: styled modal component

--- frontend/src/components/shared/Modal.jsx
+++ frontend/src/components/shared/Modal.jsx
@@ -5,10 +5,17 @@ const Modal = ({ title, onClose, isOpen, children }) => {
   return (
     <div className="fixed inset-0 bg-paleBlue-900 bg-opacity-50 flex items-center justify-center z-10">
       <div className="bg-paleBlue-800 shadow-lg w-full max-w-lg mx-4 rounded-lg p-4">
-        <div className="flex justify-between items-center px-6 py-4">
-          <h2>{title}</h2>
-          <button>&times;</button>
+        <div className="flex justify-between items-center px-6 py-4 border-b border-paleBlue-700">
+          <h2 className="text-xl text-paleBlue-100 font-semibold">{title}</h2>
+          <button
+            className="text-paleBlue-500 text-2xl hover:text-paleBlue-300"
+            onClick={onClose}
+          >
+            &times;
+          </button>
         </div>
+
+        <div className="p-6">{children}</div>
       </div>
     </div>
   );
