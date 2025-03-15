53c544574dd4792e1dd0f2b6ec3f2ea11f3c1e31
Tane4ka170 <tane4ka170@gmail.com>
2025-03-15 19:52:13 +0200

    feat: added menu and 404 exports

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,5 @@ export { default as Home } from "./Home";
 export { default as Auth } from "./Auth";
 export { default as Orders } from "./Orders";
 export { default as Tables } from "./Tables";
+export { default as Menu } from "./Menu";
+export { default as NotFound } from "./NotFound";
