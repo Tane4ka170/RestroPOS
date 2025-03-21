b40580455322d73bcc53b713ba59533c87b8aff6
Tane4ka170 <tane4ka170@gmail.com>
2025-03-21 20:33:31 +0200

    feat: displayed customer data in CustomerInfo

--- frontend/src/components/menu/CustomerInfo.jsx
+++ frontend/src/components/menu/CustomerInfo.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAvatarName } from "../../utils";
 
 const CustomerInfo = () => {
+  const customerData = useSelector((state) => state.customer);
+
   return (
     <div className="flex items-center justify-between px-4 py-3">
       <div className="flex flex-col items-start">
         <h1 className="text-md text-paleBlue-100 font-semibold tracking-wide">
-          Customer Name
+          {customerData.customerName}
         </h1>
         <p className="text-xs text-paleBlue-200 font-medium mt-1">
           #101/Dine In
@@ -15,7 +19,7 @@ const CustomerInfo = () => {
         </p>
       </div>
       <button className="bg-aquaTeal-500 p-3 text-xl font-bold rounded-lg">
-        TS
+        {getAvatarName(customerData.customerName)}
       </button>
     </div>
   );
