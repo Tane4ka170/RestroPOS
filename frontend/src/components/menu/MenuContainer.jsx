51ce31843965eeecb7e3d4c88ec8ce73a30f08d2
Tane4ka170 <tane4ka170@gmail.com>
2025-03-22 21:20:49 +0200

    feat: added add to cart logic

--- frontend/src/components/menu/MenuContainer.jsx
+++ frontend/src/components/menu/MenuContainer.jsx
@@ -18,6 +18,18 @@ const MenuContainer = () => {
     if (itemCount <= 0) return;
     setItemCount((prev) => prev - 1);
   };
+
+  const handleAddToCart = (item) => {
+    if (itemCount === 0) return;
+
+    const { name, price } = item;
+    const newObj = {
+      id: new Date(),
+      name,
+      pricePerQuantity: price,
+      quantity: itemCount,
+    };
+  };
   return (
     <>
       <div className="grid grid-cols-4 gap-4 px-10 py-4 w-[100%]">
@@ -52,37 +64,40 @@ const MenuContainer = () => {
       <hr className="border-paleBlue-400 border-t-2 mt-4" />
 
       <div className="grid grid-cols-4 gap-4 px-10 py-4 w-[100%]">
-        {selected?.items.map((menu) => {
+        {selected?.items.map((item) => {
           return (
             <div
-              key={menu.id}
+              key={item.id}
               className="flex flex-col items-start justify-between p-4 rounded-lg h-[100px] cursor-pointer hover:bg-paleBlue-500 bg-paleBlue-900"
             >
               <div className="flex items-start justify-between w-full">
                 <h1 className="text-paleBlue-100 text-lg font-semibold">
-                  {menu.name}
+                  {item.name}
                 </h1>
-                <button className="bg-green-300 text-green-600 p-2 rounded-lg">
+                <button
+                  className="bg-green-300 text-green-600 p-2 rounded-lg"
+                  onClick={() => handleAddToCart(item)}
+                >
                   <FaShoppingCart className="" size={20} />
                 </button>
               </div>
               <div className="flex items-center justify-between w-full">
                 <p className="text-paleBlue-100 text-xl font-bold">
-                  $ {menu.price}
+                  $ {item.price}
                 </p>
                 <div className="flex items-center justify-between bg-paleBlue-700 px-2 py-1 rounded-lg gap-6">
                   <button
                     className="text-aquaTeal-500 text-2xl"
-                    onClick={() => decrement(menu.id)}
+                    onClick={() => decrement(item.id)}
                   >
                     &minus;
                   </button>
                   <span className="text-paleBlue-100">
-                    {menu.id === itemId ? itemCount : "0"}
+                    {item.id === itemId ? itemCount : "0"}
                   </span>
                   <button
                     className="text-aquaTeal-500 text-2xl"
-                    onClick={() => increment(menu.id)}
+                    onClick={() => increment(item.id)}
                   >
                     &#43;
                   </button>
