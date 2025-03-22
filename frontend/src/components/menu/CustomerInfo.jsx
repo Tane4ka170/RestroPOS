a89633465d3e8b89d7879113b3c804b20ec32dd8
Tane4ka170 <tane4ka170@gmail.com>
2025-03-22 21:20:20 +0200

    feat: added date & default values

--- frontend/src/components/menu/CustomerInfo.jsx
+++ frontend/src/components/menu/CustomerInfo.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { getAvatarName } from "../../utils";
+import { formatDate, getAvatarName } from "../../utils";
 
 const CustomerInfo = () => {
   const customerData = useSelector((state) => state.customer);
+  const [dateTime] = useState(new Date());
 
   return (
     <div className="flex items-center justify-between px-4 py-3">
       <div className="flex flex-col items-start">
         <h1 className="text-md text-paleBlue-100 font-semibold tracking-wide">
-          {customerData.customerName}
+          {customerData.customerName || "Customer Name"}
         </h1>
         <p className="text-xs text-paleBlue-200 font-medium mt-1">
-          {customerData.orderId}/Dine In
+          #{customerData.orderId || "N/A"} / Dine In
         </p>
         <p className="text-xs text-paleBlue-200 font-medium mt-2">
-          March 18, 2025 20:11
+          {formatDate(dateTime)}
         </p>
       </div>
       <button className="bg-aquaTeal-500 p-3 text-xl font-bold rounded-lg">
-        {getAvatarName(customerData.customerName)}
+        {getAvatarName(customerData.customerName) || "CN"}
       </button>
     </div>
   );
