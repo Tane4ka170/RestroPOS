58a3c37954694bf712064d7e53383376c7cf3a6d
Tane4ka170 <tane4ka170@gmail.com>
2025-03-28 16:41:21 +0200

    feat: impl. global err handler logic

--- backend/middlewares/globalErrorHandler.js
+++ backend/middlewares/globalErrorHandler.js
@@ -1,3 +1,13 @@
-const globalErrorHandler = (err, res, req, next) => {};
+const config = require("../config/config");
+
+const globalErrorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  return res.status(statusCode).json({
+    status: statusCode,
+    message: err.message,
+    errorStack: config.nodeEnv === "development" ? err.stack : "",
+  });
+};
 
 module.exports = globalErrorHandler;
