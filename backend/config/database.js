487618225408c434fcb50f2ab4514920e06c802a
Tane4ka170 <tane4ka170@gmail.com>
2025-03-28 15:42:18 +0200

    feat: use config for db url

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const config = require("./config");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    const conn = await mongoose.connect(config.database_url);
     console.log(`MONGODB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error:${error.message}`);
