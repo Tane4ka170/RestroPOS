e0d3eadeb52963ad56aa8066c813defeaf3cfc28
Tane4ka170 <tane4ka170@gmail.com>
2025-03-28 15:41:34 +0200

    feat: added app config setup

---
+++ backend/config/config.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const config = Object.freeze({
+  port: process.env.PORT || 8259,
+  database_url: process.env.MONGODB_URL || "mongodb://localhost:3741",
+  node_env: process.env.NODE_ENV || "development",
+});
+
+module.exports = config;
