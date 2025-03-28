32ab02b008c0badf08ab8f0d6cbd93791b9f6fd3
Tane4ka170 <tane4ka170@gmail.com>
2025-03-28 16:40:49 +0200

    feat: added global error handling

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const connectDb = require("./config/database");
 const config = require("./config/config");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const createHttpError = require("http-errors");
 const app = express();
 
 const PORT = config.port;
 connectDb();
 
 app.get("/", (req, res) => {
+  const err = createHttpError(404, "An unexpected error occurred");
+  throw err;
+
   res.json({ message: "He said forever, but the summer died, and so did we" });
 });
+
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`POS Server is up and running on port ${PORT}`);
