aa556077245115ea26182b0a1e68b96fd7d5f132
Tane4ka170 <tane4ka170@gmail.com>
2025-03-30 15:00:13 +0300

    feat: added jwt secret to config

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,6 +4,7 @@ const config = Object.freeze({
   port: process.env.PORT || 8259,
   database_url: process.env.MONGODB_URL || "mongodb://localhost:3741",
   node_env: process.env.NODE_ENV || "development",
+  accessTokenSecret: process.env.JWT_SECRET,
 });
 
 module.exports = config;
