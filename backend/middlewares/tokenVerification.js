bd2d6e4c4b3a520b56e746d4acd668d11a09b9fe
Tane4ka170 <tane4ka170@gmail.com>
2025-03-31 19:53:25 +0300

    feat: added token verification logic

--- backend/middlewares/tokenVerification.js
+++ backend/middlewares/tokenVerification.js
@@ -1,12 +1,30 @@
 const createHttpError = require("http-errors");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const User = require("../models/userModel");
 
 const isVerifiedUser = async (req, res, next) => {
   try {
     const { accessToken } = req.cookie;
     if (!accessToken) {
       const error = createHttpError(401, "Token is required!");
+      return next(error);
     }
+
+    const decodeToken = jwt.verify(accessToken, config.accessTokenSecret);
+
+    const user = await User.findById(decodeToken._id);
+    if (!user) {
+      const error = createHttpError(401, "User does not exist!");
+      return next(error);
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
-    next(error);
+    const err = createHttpError(401, "Token is not valid!");
+    next(err);
   }
 };
+
+module.exports = { isVerifiedUser };
