44a2a1403e999c8053b75d41ace87a53047dbd7d
Tane4ka170 <tane4ka170@gmail.com>
2025-04-02 21:44:48 +0300

    feat: added order creation logic

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,21 @@
-const addOrder = async (req, res, next) => {};
+const Order = require("../models/orderModel");
+
+const addOrder = async (req, res, next) => {
+  try {
+    // const { customerDetails, orderStatus, bills, items } = req.body;
+    const order = new Order(req.body);
+    await order.save();
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Order successfully placed!",
+        data: order,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getOrder = async (req, res, next) => {};
 
