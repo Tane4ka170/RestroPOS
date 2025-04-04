08a0d16a03872aa0206f31c79cf828d313445681
Tane4ka170 <tane4ka170@gmail.com>
2025-04-04 19:12:58 +0300

    feat: added table controllers

---
+++ backend/controllers/tableController.js
@@ -0,0 +1,5 @@
+const addTable = (req, res, next) => {};
+const getTables = (req, res, next) => {};
+const updateTable = (req, res, next) => {};
+
+module.exports = { addTable, getTables, updateTable };
