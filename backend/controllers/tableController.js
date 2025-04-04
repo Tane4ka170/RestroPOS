63745fe6d5dd3415873ecc6fc522cccd47cf53bc
Tane4ka170 <tane4ka170@gmail.com>
2025-04-04 20:10:11 +0300

    feat: added table creation logic

--- backend/controllers/tableController.js
+++ backend/controllers/tableController.js
@@ -1,5 +1,35 @@
-const addTable = (req, res, next) => {};
-const getTables = (req, res, next) => {};
-const updateTable = (req, res, next) => {};
+const createHttpError = require("http-errors");
+const Table = require("../models/tableModel");
+
+const addTable = async (req, res, next) => {
+  try {
+    const { tableNo } = req.body;
+    if (!tableNo) {
+      const error = createHttpError(400, "Kindly enter the table number!");
+      return next(error);
+    }
+    const isTablePresent = await Table.findOne({ tableNo });
+    if (isTablePresent) {
+      const error = createHttpError(400, "Table already exists!");
+      return next(error);
+    }
+
+    const newTable = new Table({ tableNo });
+
+    await newTable.save();
+
+    res
+      .status(201)
+      .json({
+        success: true,
+        message: "Table successfully added!",
+        data: newTable,
+      });
+  } catch (error) {
+    next(error);
+  }
+};
+const getTables = async (req, res, next) => {};
+const updateTable = async (req, res, next) => {};
 
 module.exports = { addTable, getTables, updateTable };
