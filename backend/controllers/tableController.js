d48a70c3e540a05d625514abd78f62686bb9ecdd
Tane4ka170 <tane4ka170@gmail.com>
2025-04-05 20:47:55 +0300

    feat: added table controllers logic

--- backend/controllers/tableController.js
+++ backend/controllers/tableController.js
@@ -18,18 +18,54 @@ const addTable = async (req, res, next) => {
 
     await newTable.save();
 
-    res
-      .status(201)
-      .json({
-        success: true,
-        message: "Table successfully added!",
-        data: newTable,
-      });
+    res.status(201).json({
+      success: true,
+      message: "Table successfully added!",
+      data: newTable,
+    });
   } catch (error) {
     next(error);
   }
 };
-const getTables = async (req, res, next) => {};
-const updateTable = async (req, res, next) => {};
+const getTables = async (req, res, next) => {
+  try {
+    const tables = await Table.find().populate({
+      path: "currentOrder",
+      select: "customerDetails",
+    });
+    res.status(200).json({ success: true, data: tables });
+  } catch (error) {
+    next(error);
+  }
+};
+const updateTable = async (req, res, next) => {
+  try {
+    const { status, orderId } = req.body;
+
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      const error = createHttpError(404, "Invalid ID provided!");
+      return next(error);
+    }
+
+    const table = await Table.findByIdAndUpdate(
+      id,
+      { status, currentOrder: orderId },
+      { new: true }
+    );
+
+    if (!table) {
+      const error = createHttpError(404, "The table does not exist!");
+      return error;
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "Table has been updated!", data: table });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = { addTable, getTables, updateTable };
