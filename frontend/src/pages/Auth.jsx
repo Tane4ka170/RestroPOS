83130b1e6252222b48f31dd57cc5534f87d3b008
Tane4ka170 <tane4ka170@gmail.com>
2025-04-06 19:40:11 +0300

    feat: added auth page ui

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import restaurant from "../assets/images/restoraunt.jpg";
+import logo from "../assets/images/logo.png";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="flex min-h-screen w-full">
+      {/* Left section */}
+      <div className="w-1/2 relative flex items-center justify-center bg-cover">
+        {/* Bg img */}
+        <img
+          src={restaurant}
+          alt="Restaurant Imag"
+          className="w-full h-full object-cover"
+        />
+
+        {/* Black overlay */}
+        <div className="absolute inset-0 bg-[#000] bg-opacity-80"></div>
+
+        {/* Quote at bottom */}
+        <blockquote className="absolute bottom-10 px-8 mb-10 text-2xl italic text-paleBlue-100">
+          "Offer customers delicious food, quick and friendly service, and a
+          warm atmosphere — and they'll return time and again"
+          <br />
+          <span className="block mt-4 text-aquaTeal-600">
+            - Founder of Restro
+          </span>
+        </blockquote>
+      </div>
+
+      {/* Right section */}
+      <div className="w-1/2 min-h-screen bg-paleBlue-900 p-10">
+        <div className="flex flex-col items-center gap-2">
+          <img
+            src={logo}
+            alt="logo"
+            className="h-14 w-14 border-2 rounded-full p-1"
+          />
+          <h1 className="text-lg font-semibold text-paleBlue-100 tracking-wide">
+            Restro
+          </h1>
+        </div>
+
+        <h2 className="text-4xl text-center mt-10 font-semibold"></h2>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
