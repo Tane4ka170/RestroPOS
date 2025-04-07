eb1f05f790c7e079494603444cc4bc8f44b71ec9
Tane4ka170 <tane4ka170@gmail.com>
2025-04-07 21:02:24 +0300

    feat: added http client and api functions

---
+++ frontend/src/https/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+export const login = (data) => api.post("api/user/login", data);
+export const register = (data) => api.post("api/user/register", data);
+export const getUserData = () => api.get("api/user");
