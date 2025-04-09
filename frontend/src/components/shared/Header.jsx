960666c85d39ca01a85985376d5aca7255f15742
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-09 19:20:10 +0300

    feat: added header user info and logout

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import logo from "../../assets/images/logo.png";
 import { FaRegUser, FaSearch } from "react-icons/fa";
-import { CiBellOn } from "react-icons/ci";
+import { CiBellOn, CiLogout } from "react-icons/ci";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { useMutation } from "@tanstack/react-query";
+import { logout } from "../../https";
+import { removeUser } from "../../redux/slices/userSlice";
 
 const Header = () => {
+  const userData = useSelector((state) => state.user);
+
+  console.log("User data: ", userData);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutMutation = useMutation({
+    mutationFn: () => logout(),
+    onSuccess: (data) => {
+      console.log("Logout success data: ", data);
+      dispatch(removeUser());
+      navigate("/auth");
+    },
+    onError: (error) => {
+      console.log("Logout error: ", error);
+    },
+  });
+
+  const handleLogout = () => {
+    console.log("Logging out...");
+    logoutMutation.mutate();
+  };
   return (
     <header className="flex justify-between items-center py-4 px-8 bg-paleBlue-500">
       {/* Logo */}
@@ -31,9 +58,16 @@ const Header = () => {
           <FaRegUser className="text-paleBlue-100 text-4xl" />
           <div className="flex flex-col items-start">
             <h1 className="text-md text-paleBlue-100 font-semibold">
-              Taylor Swift
+              {userData.name || "Test"}
             </h1>
-            <p className="text-xs text-paleBlue-300">Admin</p>
+            <p className="text-xs text-paleBlue-300">
+              {userData.role || "N/A"}
+            </p>
+            <CiLogout
+              onClick={handleLogout}
+              className="text-paleBlue-200 ml-2"
+              size={40}
+            />
           </div>
         </div>
       </div>
