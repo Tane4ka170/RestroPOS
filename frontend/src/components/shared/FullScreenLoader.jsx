983055df37225236fe49702dd26d8e24ffd1b694
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-12 16:25:32 +0300

    feat: added full screen loader component

---
+++ frontend/src/components/shared/FullScreenLoader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FullScreenLoader = () => {
+  return (
+    <div className="fullScreenLoader">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default FullScreenLoader;
