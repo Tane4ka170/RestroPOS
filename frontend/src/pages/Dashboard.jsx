672abd9bd4af222784a6dc09839d747059abee0d
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-13 16:28:25 +0300

    feat: added tab navigation to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiDish } from "react-icons/bi";
 import { MdCategory, MdOutlineTableBar } from "react-icons/md";
 import Metrics from "../components/dashboard/Metrics";
+import RecentOrders from "../components/dashboard/RecentOrders";
 
 const buttons = [
   { label: "Add Table", icon: <MdOutlineTableBar />, action: "table" },
@@ -12,6 +13,7 @@ const buttons = [
 const tabs = ["Metrics", "Orders", "Payments"];
 
 const Dashboard = () => {
+  const [activeTab, setActiveTab] = useState("Metrics");
   return (
     <div className="bg-paleBlue-400 h-[calc(100vh-5rem)]">
       <div className="container mx-auto flex items-center justify-between py-14 px-6 md:px-4">
@@ -31,16 +33,22 @@ const Dashboard = () => {
           {tabs.map((tab) => {
             return (
               <button
-                className="bg-paleBlue-100 hover:bg-paleBlue-300 px-8 py-3 rounded-lg text-paleBlue-400 font-semibold text-md flex items-center gap-2"
+                className={` px-8 py-3 rounded-lg text-paleBlue-400 font-semibold text-md flex items-center gap-2 ${
+                  activeTab === tab
+                    ? "bg-paleBlue-300"
+                    : "bg-paleBlue-100 hover:bg-paleBlue-300"
+                }`}
                 key={tab}
+                onClick={() => setActiveTab(tab)}
               >
                 {tab}
               </button>
             );
           })}
         </div>
-        <Metrics />
       </div>
+      {activeTab === "Metrics" && <Metrics />}
+      {activeTab === "Orders" && <RecentOrders />}
     </div>
   );
 };
