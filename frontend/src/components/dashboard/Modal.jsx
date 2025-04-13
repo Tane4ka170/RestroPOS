7c39802ac0caba4c0aaa73fe7cd171868967007b
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-13 17:31:53 +0300

    feat: added table creation modal

---
+++ frontend/src/components/dashboard/Modal.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { IoIosClose } from "react-icons/io";
+
+const Modal = ({ setIsTableModalOpen }) => {
+  const handleCloseModal = () => {
+    setIsTableModalOpen(false);
+  };
+  return (
+    <div className="fixed inset-0 bg-paleBlue-900 bg-opacity-50 flex items-center justify-center z-50">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0 }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
+        className="bg-paleBlue-800 p-6 rounded-lg shadow-lg w-96"
+      >
+        {/* Modal Header */}
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-paleBlue-100 text-xl font-semibold">Add Table</h2>
+          <button
+            className="text-paleBlue-700 hover:text-green-900"
+            onClick={handleCloseModal}
+          >
+            <IoIosClose size={24} />
+          </button>
+        </div>
+
+        {/* Modal Body */}
+        <form className="space-y-4 mt-10">
+          <div>
+            <label className="block text-paleBlue-200 mb-2 mt-3 text-sm font-medium">
+              Table Number
+            </label>
+            <div className="flex items-center rounded-lg p-5 px-4 bg-paleBlue-200">
+              <input
+                type="number"
+                name="tableNo"
+                className="bg-transparent flex-1 text-paleBlue-800 focus:outline-none"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-paleBlue-200 mb-2 mt-3 text-sm font-medium">
+              Number of Seats
+            </label>
+            <div className="flex items-center rounded-lg p-5 px-4 bg-paleBlue-200">
+              <input
+                type="number"
+                name="seats"
+                className="bg-transparent flex-1 text-paleBlue-800 focus:outline-none"
+                required
+              />
+            </div>
+          </div>
+        </form>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Modal;
