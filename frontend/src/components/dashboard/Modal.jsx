fe9009868da1d0b881f1692ff939b7219b7453d5
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-14 19:08:30 +0300

    feat: integrated table creation API in modal

--- frontend/src/components/dashboard/Modal.jsx
+++ frontend/src/components/dashboard/Modal.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { IoIosClose } from "react-icons/io";
+import { useMutation } from "@tanstack/react-query";
+import { addTable } from "../../https";
+import { enqueueSnackbar } from "notistack";
 
 const Modal = ({ setIsTableModalOpen }) => {
+  const [tableData, setTableData] = useState({ tableNo: "", seats: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTableData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(tableData);
+    tableMutation.mutate(tableData);
+  };
   const handleCloseModal = () => {
     setIsTableModalOpen(false);
   };
+
+  const tableMutation = useMutation({
+    mutationFn: (reqData) => addTable(reqData),
+    onSuccess: (data) => {
+      setIsTableModalOpen(false), console.log(data);
+    },
+    onError: (error) => {
+      const { data } = error.response;
+      console.log(error);
+      enqueueSnackbar(data.message, { variants: "error" });
+    },
+  });
   return (
     <div className="fixed inset-0 bg-paleBlue-900 bg-opacity-50 flex items-center justify-center z-50">
       <motion.div
@@ -27,7 +54,7 @@ const Modal = ({ setIsTableModalOpen }) => {
         </div>
 
         {/* Modal Body */}
-        <form className="space-y-4 mt-10">
+        <form className="space-y-4 mt-10" onSubmit={handleSubmit}>
           <div>
             <label className="block text-paleBlue-200 mb-2 mt-3 text-sm font-medium">
               Table Number
@@ -38,6 +65,8 @@ const Modal = ({ setIsTableModalOpen }) => {
                 name="tableNo"
                 className="bg-transparent flex-1 text-paleBlue-800 focus:outline-none"
                 required
+                value={tableData.tableNo}
+                onChange={handleInputChange}
               />
             </div>
           </div>
@@ -51,9 +80,18 @@ const Modal = ({ setIsTableModalOpen }) => {
                 name="seats"
                 className="bg-transparent flex-1 text-paleBlue-800 focus:outline-none"
                 required
+                value={tableData.seats}
+                onChange={handleInputChange}
               />
             </div>
           </div>
+
+          <button
+            className="w-full mt-6 py-3 text-lg bg-aquaTeal-600 text-paleBlue-200 font-bold"
+            type="submit"
+          >
+            Add Table
+          </button>
         </form>
       </motion.div>
     </div>
