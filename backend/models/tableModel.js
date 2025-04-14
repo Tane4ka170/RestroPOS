a1e9d9ca9c13cd4e1b854b8a9ac876298554d408
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-14 19:08:04 +0300

    feat: added seats field to table model

--- backend/models/tableModel.js
+++ backend/models/tableModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const tableModel = new mongoose.Schema({
   tableNo: { type: Number, required: true, unique: true },
   status: { type: String, required: true, default: "Available" },
+  seats: {
+    type: Number,
+    required: true,
+  },
   currentOrder: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
 });
 
