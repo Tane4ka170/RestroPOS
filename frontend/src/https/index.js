109bbb9f9456d4b14e46e7f79f0058eefee8f498
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-17 19:05:05 +0300

    feat: added stripe order API call

--- frontend/src/https/index.js
+++ frontend/src/https/index.js
@@ -16,3 +16,6 @@ export const logout = () => api.post("/api/user/logout");
 
 export const addTable = (data) => api.post("/api/table", data);
 export const getTables = () => api.get("/api/table");
+
+export const createOrderStripe = (data) =>
+  api.post("/api/payment/create-order", data);
