a08ff098d4d21800bb21965bf2d154a297940aa4
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-18 18:33:38 +0300

    fix: corrected mongoose import

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require("mongoose");
+const { mongoose } = require("mongoose");
 const Order = require("../models/orderModel");
 const createHttpError = require("http-errors");
 
