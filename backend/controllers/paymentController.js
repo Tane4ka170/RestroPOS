e52d7e6ab042ddfb7d9d6b46a92930189fde55b9
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-18 18:34:19 +0300

    refactor: moved stripe init to controller

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,23 +1,26 @@
+const config = require("../config/config");
 const stripe = require("stripe")(config.stripeSecretKey);
 
-try {
-  const { amount } = req.body;
+const createOrder = async (req, res, next) => {
+  try {
+    const { amount } = req.body;
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: amount * 100,
-    currency: "usd",
-    metadata: {
-      integration_check: "accept_a_payment",
-    },
-  });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: "usd",
+      metadata: {
+        integration_check: "accept_a_payment",
+      },
+    });
 
-  res.json({
-    success: true,
-    clientSecret: paymentIntent.client_secret,
-    amount: paymentIntent.amount,
-  });
-} catch (error) {
-  next(error);
-}
+    res.json({
+      success: true,
+      clientSecret: paymentIntent.client_secret,
+      amount: paymentIntent.amount,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = { createOrder };
