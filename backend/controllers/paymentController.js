a07f733004b663a283176e863ba17639816fd0d2
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-20 19:56:24 +0300

    feat: implemented stripe checkout sessions

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -5,18 +5,28 @@ const createOrder = async (req, res, next) => {
   try {
     const { amount } = req.body;
 
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount * 100,
-      currency: "usd",
-      metadata: {
-        integration_check: "accept_a_payment",
-      },
-    });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: "Order Payment",
+            },
+            unit_amount: Math.round(amount * 100), 
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: "http://localhost:5173/",
+      cancel_url: "http://localhost:5173/,
+    })
 
     res.json({
       success: true,
-      clientSecret: paymentIntent.client_secret,
-      amount: paymentIntent.amount,
+      sessionId: session.id,
     });
   } catch (error) {
     next(error);
