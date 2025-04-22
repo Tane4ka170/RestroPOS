214714f45d229698e9ce3b27cb9259e6905bf74f
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-22 18:40:49 +0300

    feat: impl. stripe webhook for payment

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -1,5 +1,6 @@
 const config = require("../config/config");
 const stripe = require("stripe")(config.stripeSecretKey);
+const Payment = require("../models/paymentModel");
 
 const createOrder = async (req, res, next) => {
   try {
@@ -15,14 +16,14 @@ const createOrder = async (req, res, next) => {
             product_data: {
               name: "Order Payment",
             },
-            unit_amount: Math.round(amount * 100), 
+            unit_amount: Math.round(amount * 100),
           },
           quantity: 1,
         },
       ],
       success_url: "http://localhost:5173/",
-      cancel_url: "http://localhost:5173/,
-    })
+      cancel_url: "http://localhost:5173/",
+    });
 
     res.json({
       success: true,
@@ -33,4 +34,40 @@ const createOrder = async (req, res, next) => {
   }
 };
 
-module.exports = { createOrder };
+const stripeWebhook = async (req, res, next) => {
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.rawBody, sig, endpointSecret);
+  } catch (err) {
+    console.error("Webhook Error:", err.message);
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object;
+
+    const newPayment = new Payment({
+      paymentId: session.payment_intent,
+      orderId: session.id,
+      amount: session.amount_total / 100,
+      currency: session.currency,
+      status: session.payment_status,
+      email: session.customer_details.email,
+      createdAt: new Date(),
+    });
+
+    try {
+      await newPayment.save();
+      console.log("✅ Оплата збережена в базу");
+    } catch (err) {
+      console.error("❌ Помилка при збереженні:", err);
+    }
+  }
+
+  res.json({ received: true });
+};
+
+module.exports = { createOrder, stripeWebhook };
