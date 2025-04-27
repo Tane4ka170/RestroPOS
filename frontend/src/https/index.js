084ea5a2c95c03fe243099fefe28c30900499703
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-04-27 17:17:52 +0300

    feat: added updateOrderStatus API call

--- frontend/src/https/index.js
+++ frontend/src/https/index.js
@@ -24,3 +24,5 @@ export const createOrderStripe = (data) =>
 
 export const addOrder = (data) => api.post("/api/order/", data);
 export const getOrders = () => api.get("/api/order");
+export const updateOrderStatus = ({ orderId, ...orderStatus }) =>
+  api.put(`/api/order/${orderId}`, { orderStatus });
