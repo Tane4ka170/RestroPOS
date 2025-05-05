5b0958f93c4ffb369444abd782aceeda2304f887
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-05-05 15:59:34 +0300

    refactor: OrderCard UI

--- frontend/src/components/orders/OrderCard.jsx
+++ frontend/src/components/orders/OrderCard.jsx
@@ -6,44 +6,44 @@ const OrderCard = ({ key, order }) => {
   return (
     <div className="w-full max-w-[400px] bg-paleBlue-600 p-4 rounded-lg mb-4">
       <div className="flex items-center gap-5">
-        <button className="bg-aquaTeal-500  p-3 text-xl font-bold rounded-lg">
+        <button className="p-3 text-xl font-bold rounded-lg bg-aquaTeal-500">
           {getAvatarName(order.customerDetails.name)}
         </button>
 
         <div className="flex items-center justify-between w-[100%]">
           <div className="flex flex-col items-start gap-1">
-            <h1 className="text-paleBlue-100 text-lg font-semibold tracking-wide">
+            <h1 className="text-lg font-semibold tracking-wide text-paleBlue-100">
               {order.customerDetails.name}
             </h1>
-            <p className="text-paleBlue-200 text-sm">
+            <p className="text-sm text-paleBlue-200">
               #{Math.floor(new Date(order._id).getTime())}/Dine in
             </p>
-            <p className="text-paleBlue-200 text-sm">
+            <p className="text-sm text-paleBlue-200">
               Table{" "}
               <FaArrowAltCircleRight className="inline ml-2 text-paleBlue-700" />{" "}
-              {order.table.tableNo}
+              {order.table?.tableNo || "Unknown"}
             </p>
           </div>
 
           <div className="flex flex-col items-end gap-2">
             {order.orderStatus === "Ready" ? (
               <>
-                <p className="text-green-600 px-2 bg-green-300 rounded-lg py-1">
-                  <FaCheck className="mr-2 inline" />
+                <p className="px-2 py-1 text-green-600 bg-green-300 rounded-lg">
+                  <FaCheck className="inline mr-2" />
                   {order.orderStatus}
                 </p>
-                <p className="text-paleBlue-200 text-sm">
+                <p className="text-sm text-paleBlue-200">
                   <FaCircle className="inline mr-2 text-green-600" /> Ready to
                   serve
                 </p>
               </>
             ) : (
               <>
-                <p className="text-green-600 px-2 bg-aquaTeal-200 rounded-lg py-1">
-                  <FaCircle className="mr-2 inline" />
+                <p className="px-2 py-1 text-green-600 rounded-lg bg-aquaTeal-200">
+                  <FaCircle className="inline mr-2" />
                   {order.orderStatus}
                 </p>
-                <p className="text-paleBlue-200 text-sm">
+                <p className="text-sm text-paleBlue-200">
                   <FaCircle className="inline mr-2 text-green-600" /> Getting
                   your order ready
                 </p>
@@ -53,15 +53,15 @@ const OrderCard = ({ key, order }) => {
         </div>
       </div>
 
-      <div className="flex justify-between items-center mt-4 text-paleBlue-200">
+      <div className="flex items-center justify-between mt-4 text-paleBlue-200">
         <p>{formatDateAndTime(order.createdAt)}</p>
         <p>{order.items.length} items</p>
       </div>
-      <hr className="w-full text-paleBlue-100 mt-4 border-t-1 border-royalBlue-500" />
+      <hr className="w-full mt-4 text-paleBlue-100 border-t-1 border-royalBlue-500" />
       <div className="flex items-center justify-between mt-4">
-        <h1 className="text-paleBlue-100 text-lg font-semibold">Total</h1>
-        <p className="text-paleBlue-100 text-lg font-semibold">
-          ${order.bulls.totalWithTax.toFixed(2)}
+        <h1 className="text-lg font-semibold text-paleBlue-100">Total</h1>
+        <p className="text-lg font-semibold text-paleBlue-100">
+          ${order.bulls?.totalWithTax?.toFixed(2) || "0.00"}
         </p>
       </div>
     </div>
