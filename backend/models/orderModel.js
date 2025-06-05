1c3dc5ee7161b3134c242bc6fb48e76f6902cfde
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2025-06-05 19:37:42 +0300

    Feat: add Table ref to order model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -21,7 +21,7 @@ const orderSchema = new mongoose.Schema(
       totalWithTax: { type: Number, required: true },
     },
     items: [],
-    table: { type: mongoose.Schema.Types.ObjectId },
+    table: { type: mongoose.Schema.Types.ObjectId, ref: "Table" },
   },
   { timestamps: true }
 );
